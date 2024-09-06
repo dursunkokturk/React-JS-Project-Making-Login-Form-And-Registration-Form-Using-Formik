0a7ef0849bd3d2a294983426a8642cfe0344859c
Dursun Köktürk <dursunserefkokturk@gmail.com>
2024-09-06 13:34:33 +0300

    login-form-and-registration-form-construction-using-formik-and-yup-validation

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// React-Router-Dom Library Icindeki
-// BrowserRouter Paketini Kullaniyoruz
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  /* React Router i Kullanabilmek Icin
-      App Componentini 
-      BrowserRouter Componenti Icine Almak Gerekiyor */
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
 
 reportWebVitals();
